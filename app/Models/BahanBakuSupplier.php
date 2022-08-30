e9c0088e5f389014cf247ccb0c64c41a63821013
thype5209 <44895587+thype5209@users.noreply.github.com>
2022-08-30 18:27:07 +0800

    Make BahanBakuSupplier Models

---
+++ app/Models/BahanBakuSupplier.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BahanBakuSupplier extends Model
+{
+    use HasFactory;
+}
