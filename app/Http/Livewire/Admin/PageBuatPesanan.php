92577c313c8d4897b82fb9eb0d7e23c395210bef
thype5209 <44895587+thype5209@users.noreply.github.com>
2022-09-27 16:54:48 +0800

    Update PageBuatPesanan.php

--- app/Http/Livewire/Admin/PageBuatPesanan.php
+++ app/Http/Livewire/Admin/PageBuatPesanan.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Admin;
 
 use Livewire\Component;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class PageBuatPesanan extends Component
 {
@@ -10,4 +11,24 @@ class PageBuatPesanan extends Component
     {
         return view('livewire.admin.page-buat-pesanan');
     }
+    public $itemAdd = false , $itemEdit = false, $itemDelete = false , $itemID;
+
+    public function addModal(){
+        $this->itemAdd = true;
+
+        Alert::success("Info", "Berhasil Di Tambah");
+    }
+    public function editModal($id){
+        $this->itemID = $id;
+
+        Alert::success("Info", "Berhasil Di Edit");
+        $this->itemEdit = true;
+    }
+    public function deleteModal($id){
+        $this->itemID = $id;
+
+        Alert::success("Info", "Berhasil Di Hapus");
+        $this->itemDelete = true;
+    }
+
 }
