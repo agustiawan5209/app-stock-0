916bd658148c47a94a146745087a670b670a3eab
thype5209 <44895587+thype5209@users.noreply.github.com>
2022-09-27 16:54:43 +0800

    Update modal.blade.php

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,40 +1,45 @@
 <section>
-     {{-- Tambah --}}
- <x-jet-dialog-modal wire:model='itemAdd'>
-    <x-slot name="title">
-        Tambah Satuan
-    </x-slot>
-    <x-slot name="content">
-        {{ $modal_dialog }}
-    </x-slot>
-    <x-slot name="footer">
-        <x-jet-danger-button type="button" wire:click="$toggle('itemAdd')">Tutup</x-jet-danger-button>
-    </x-slot>
-</x-jet-dialog-modal>
-{{-- Edit --}}
-<x-jet-dialog-modal wire:model='itemEdit'>
-    <x-slot name="title">
-        Edit Satuan
-    </x-slot>
-    <x-slot name="content">
-        {{ $modal_edit }}
-    </x-slot>
-    <x-slot name="footer">
-        <x-jet-button type="button" wire:click="$toggle('itemEdit')">Simpan</x-jet-button>
-    </x-slot>
-</x-jet-dialog-modal>
-{{-- Delete --}}
-<div>
-    <x-jet-confirmation-modal wire:model='itemDelete'>
+    {{-- Tambah --}}
+    <x-jet-dialog-modal wire:model='itemAdd' maxWidth="md">
         <x-slot name="title">
-            Delete Satuan
+            Tambah Satuan
         </x-slot>
         <x-slot name="content">
-            {{ $modal_delete }}
+            {{ $modal_dialog }}
         </x-slot>
         <x-slot name="footer">
-            <x-jet-button type="button" wire:click="$toggle('itemDelete')">Simpan</x-jet-button>
+            <x-jet-button type="button" wire:click="create">Tambah</x-jet-button>
+            <x-jet-danger-button type="button" wire:click="$toggle('itemAdd')">Tutup</x-jet-danger-button>
         </x-slot>
-    </x-jet-confirmation-modal>
-</div>
+    </x-jet-dialog-modal>
+    {{-- Edit --}}
+    <x-jet-dialog-modal wire:model='itemEdit'>
+        <x-slot name="title">
+            Edit Satuan
+        </x-slot>
+        <x-slot name="content">
+            {{ $modal_edit }}
+        </x-slot>
+        <x-slot name="footer">
+
+            <x-jet-danger-button type="button" wire:click="$toggle('itemEdit')">tutup</x-jet-danger-button>
+        </x-slot>
+    </x-jet-dialog-modal>
+    {{-- Delete --}}
+    <div>
+        <x-jet-confirmation-modal wire:model='itemDelete'>
+            <x-slot name="title">
+                Delete Satuan
+            </x-slot>
+            <x-slot name="content">
+                {{ $modal_delete }}
+            </x-slot>
+            <x-slot name="footer">
+            {{-- @if ($itemID != null)
+                    <x-jet-button type="button" wire:click="delete({{ $itemID }})">Delete</x-jet-button>
+            @endif --}}
+                <x-jet-danger-button type="button" wire:click="$toggle('itemDelete')">tutup</x-jet-danger-button>
+            </x-slot>
+        </x-jet-confirmation-modal>
+    </div>
 </section>
