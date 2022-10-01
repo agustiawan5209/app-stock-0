6acda933f53826e471dc4031361b03968bf27359
thype5209 <44895587+thype5209@users.noreply.github.com>
2022-10-01 19:42:22 +0800

    update Item

--- resources/views/components/td.blade.php
+++ resources/views/components/td.blade.php
@@ -1,3 +1,3 @@
-<td {{$attributes->merge(['class'=>'py-3 px-6 text-center border-x border-gray-200 '])}}>
+<td {{$attributes->merge(['class'=>'py-1 px-2 text-center border-x border-gray-200 '])}}>
     {{$slot}}
 </td>
