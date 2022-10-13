2a11bd20ca569a64db4ae1b25ce56aa83bde43ed
thype5209 <44895587+thype5209@users.noreply.github.com>
2022-10-13 16:49:28 +0800

    Update Welcome

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,8 +10,7 @@
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
-    <link rel="stylesheet" href="{{asset('build/assets/app.829b7516.css')}}">
-    <script src="{{asset('build/assets/app.ab93cf8a.js')}}"></script>
+    @vite(['resources/css/app.css','resources/js/app.js'])
     <script src="{{asset('js/core.js')}}"></script>
 
     <!-- Styles -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,47 +1,92 @@
 <x-guest-layout>
-    {{-- Image by <a href="https://pixabay.com/users/silviarita-3142410/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2023602">Silvia</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2023602">Pixabay</a> --}}
-    <div class="hero min-h-screen" style="background-image: url({{asset('img/buah/pomegranate-g7262827a5_1920.jpg')}});" >
+    {{-- Image by <a
+        href="https://pixabay.com/users/silviarita-3142410/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2023602">Silvia</a>
+    from <a
+        href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2023602">Pixabay</a>
+    --}}
+    <div class="hero min-h-screen"
+        style="background-image: url({{asset('img/buah/pomegranate-g7262827a5_1920.jpg')}});">
         <div class="hero-overlay bg-opacity-60"></div>
         <div class="hero-content text-center text-neutral-content">
-          <div class="max-w-md">
-            <h1 class="mb-5 text-5xl font-bold" id="textTyp">MARKISA MEGABUANA</h1>
-            <p class="mb-5 font-bold md:text-lg capitalize text-white shadow-sm"> produk minuman khas dari Kota Makassar yang sangat cocok dijadikan oleh-oleh atau bingkisan untuk sanak keluarga, teman dan para handai taulan.</p>
-            <button class="btn btn-primary">Dapatkan Skrg</button>
-          </div>
+            <div class="max-w-md">
+                <h1 class="mb-5 text-5xl font-bold" id="textTyp">MARKISA MEGABUANA</h1>
+                <p class="mb-5 font-bold md:text-lg capitalize text-white shadow-sm"> produk minuman khas dari Kota
+                    Makassar yang sangat cocok dijadikan oleh-oleh atau bingkisan untuk sanak keluarga, teman dan para
+                    handai taulan.</p>
+                <button class="btn btn-primary">Dapatkan Skrg</button>
+            </div>
         </div>
-      </div>
-     <div class="container px-3 md:px-5 mx-auto text-end flex justify-center items-center">
+    </div>
+    <div class="container px-3 md:px-5 mx-auto text-end flex flex-col md:flex-row justify-center items-center">
         <div class="hero min-h-screen bg-base-200 w-1/2">
             <div class="hero-content flex-col lg:flex-row-reverse">
-                {{-- Image by <a href="https://pixabay.com/users/adrianohermini-3005030/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1555468">Adriano Hermini</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1555468">Pixabay</a> --}}
-              <img src="{{asset('img/buah/passion-fruit-g123d090c6_1920.jpg')}}" class="max-w-xs rounded-lg shadow-2xl" />
-              <div>
-                <h1 class="text-5xl font-bold">Produk Terbaik</h1>
-                <p class="py-6">Diolah dari buah Markisa pilihan terbaik, Markisa Mega Buana merupakan satu dari sedikit produk buah markisa dari Makassar yang memberikan sensasi rasa markisa paling berkualitas, kental dan nikmat dikonsumsi kapan saja.</p>
-                <button class="btn btn-primary">Dapatkan Skrg</button>
-              </div>
+                {{-- Image by <a
+                    href="https://pixabay.com/users/adrianohermini-3005030/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1555468">Adriano
+                    Hermini</a> from <a
+                    href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1555468">Pixabay</a>
+                --}}
+                <img src="{{asset('img/buah/passion-fruit-g123d090c6_1920.jpg')}}"
+                    class="max-w-xs rounded-lg shadow-2xl" />
+                <div>
+                    <h1 class="text-5xl font-bold">Produk Terbaik</h1>
+                    <p class="py-6">Diolah dari buah Markisa pilihan terbaik, Markisa Mega Buana merupakan satu dari
+                        sedikit produk buah markisa dari Makassar yang memberikan sensasi rasa markisa paling
+                        berkualitas, kental dan nikmat dikonsumsi kapan saja.</p>
+                    <button class="btn btn-primary">Dapatkan Skrg</button>
+                </div>
             </div>
-          </div>
-          <div class="card w-96 bg-primary text-primary-content text-end">
+        </div>
+        <div class="card w-96 bg-primary text-primary-content text-end">
             <div class="card-body">
-              <h2 class="card-title text-end">Telah Bertahan Lama</h2>
-              <p class="text-start">Sirup Markisa UD. Mega Buana Berdiri Sejak Tahun 1999 Di Kota Makassar</p>
-              <div class="card-actions justify-end">
-                <button class="btn">Beli Sekarang</button>
-              </div>
+                <h2 class="card-title text-end">Telah Bertahan Lama</h2>
+                <p class="text-start">Sirup Markisa UD. Mega Buana Berdiri Sejak Tahun 1999 Di Kota Makassar</p>
+                <div class="card-actions justify-end">
+                    <button class="btn">Beli Sekarang</button>
+                </div>
             </div>
-          </div>
-     </div>
-     {{-- <div class="artboard artboard-horizontal phone-6">
-        <div class="card w-96 bg-primary text-primary-content">
-            <div class="card-body">
-              <h2 class="card-title">Card title!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div class="card-actions justify-end">
-                <button class="btn">Beli Sekarang</button>
-              </div>
+        </div>
+    </div>
+    <div class="w-full max-h-full block card">
+        <div class="flex justify-center card-body items-center text-center ">
+        <div class="text-center text-base md:text-2xl lg:text-3xl font-bold underline">Khasiat dan Manfaat Buah Markisa</div>
+            <div class="max-w-lg text-justify">
+                <p class="text-base md:text-lg lg:text-2xl ">
+                    Markisa merupakan salah satu buah yang memiliki banyak manfaat bagi kesehatan tubuh karena kaya akan
+                vitamin
+                dan sejumlah mineral penting.
+
+                Mengkonsumsi buah Markisa, atau produk olahan dari buah Markisa berwarna kuning secara rutin dan teratur
+                dapat memberikan manfaat sebagai berikut:
+                </p>
+            <ul type="1" class=" list-decimal pl-4">
+                <li>Mencegah Pertumbuhan Sel Kanker</li>
+                <li>Memperlancar dan Menyehatkan Pencernaan</li>
+                <li>Memiliki Efek Relaksasi</li>
+                <li> Baik untuk Penderita Insomnia</li>
+                <li> Baik untuk Penderita Asma</li>
+                <li>Sumber Nutrisi yang Baik</li>
+                <li>Mengoptimalkan Sistem Imunitas</li>
+                <li>Mengontrol Tekanan Darah</li>
+                <li>Baik untuk Diet</li>
+                <li> Baik dalam Memenuhi Gizi Tubuh</li>
+                <li>Baik untuk Kesehatan Mental dan Otak</li>
+                <li>Baik untuk Kesehatan Kulit</li>
+                <li>Mampu Mendorong Pertumbuhan Tulang dan Gigi</li>
+                <li> Mencegah Infeksi</li>
+                <li>Menjaga Kesehatan Jantung</li>
+            </ul>
             </div>
-          </div>
-     </div> --}}
+        </div>
+    </div>
+    <div class="w-full max-h-full block mt-4 card ">
+        <div class="flex justify-center ">
+            <div class="max-w-xl card-body items-center text-center">
+                <div class="text-base md:text-2xl lg:text-3xl  font-bold underline card-title">Cara Order Sari Markisa MegaBuana Makassar</div>
+                <p class="text-base md:text-lg lg:text-2xl ">
+                    Untuk memesan produk Sari Buah Markisa Karaeng 100% ASLI dari Makassar, silahkan hubungi Telp. 0822-9059-1298 setiap hari kerja.
+                </p>
+            </div>
+        </div>
+    </div>
 
 </x-guest-layout>
