7bbb14d7eb95124ad579c717af18d9bcaf1e4bff
thype5209 <44895587+thype5209@users.noreply.github.com>
2022-10-17 14:56:39 +0800

    Update Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
             <img src="{{asset('img/landing/landing (3).jpeg')}}" class="max-w-sm rounded-lg shadow-2xl" />
             <div>
                 <h1 class="text-5xl font-bold">UD. Mega Buana</h1>
-                <p class="py-6">Markisa MEGA BUANA adalah produk minuman khas dari Kota Makassar yang sangat cocok dijadikan oleh-oleh atau bingkisan untuk sanak keluarga, teman dan para handai taulan.</p>
+                <p class="py-6">Markisa MEGA BUANA adalah produk minuman khas dari Kota Makassar yang sangat cocok
+                    dijadikan oleh-oleh atau bingkisan untuk sanak keluarga, teman dan para handai taulan.</p>
                 <button class="btn btn-primary">Get Started</button>
             </div>
         </div>
@@ -32,144 +33,116 @@
         </div>
     </section> --}}
 
-<!-- Pazly Blocks Ends Here -->
-<section>
-    <div class="mx-auto max-w-screen-2xl px-4 py-8 sm:px-6 lg:px-8">
-        <div class="grid grid-cols-1 gap-4 md:grid-cols-2">
-            <div class="bg-blue-600 p-8 md:p-12 lg:px-16 lg:py-24">
-                <div class="mx-auto max-w-xl text-center">
-                    <h2 class="text-sm font-bold text-white md:text-lg">
-                        Diolah dari buah Markisa pilihan terbaik, Markisa MEGA BUANA merupakan satu dari sedikit produk SARI markisa  yang memberikan sensasi rasa markisa paling berkualitas, kental dan nikmat dikonsumsi kapan saja.
-                    </h2>
+    <!-- Pazly Blocks Ends Here -->
+    <section>
+        <div class="mx-auto max-w-screen-2xl px-4 py-8 sm:px-6 lg:px-8">
+            <div class="grid grid-cols-1 gap-4 md:grid-cols-2">
+                <div class="bg-blue-600 p-8 md:p-12 lg:px-16 lg:py-24">
+                    <div class="mx-auto max-w-xl text-center">
+                        <h2 class="text-sm font-bold text-white md:text-lg">
+                            Diolah dari buah Markisa pilihan terbaik, Markisa MEGA BUANA merupakan satu dari sedikit
+                            produk SARI markisa yang memberikan sensasi rasa markisa paling berkualitas, kental dan
+                            nikmat dikonsumsi kapan saja.
+                        </h2>
 
-                    <p class="hidden text-white/90 sm:mt-4 sm:block">
-                        Pengembangan usaha dari UD. Mega berkaitan erat dengan minuman
-                        sehingga diperlukan juga izin resmi dari Departemen Kesehatan yang
-                        menyatakan bahwa produk Markisa tersebut dapat dikonsumsi oleh masyarakat,
-                        dengan nomor : HK.00.05.5.1640 pada tanggal 30 April 2003
-                    </p>
+                        <p class="hidden text-white/90 sm:mt-4 sm:block">
+                            Pengembangan usaha dari UD. Mega berkaitan erat dengan minuman
+                            sehingga diperlukan juga izin resmi dari Departemen Kesehatan yang
+                            menyatakan bahwa produk Markisa tersebut dapat dikonsumsi oleh masyarakat,
+                            dengan nomor : HK.00.05.5.1640 pada tanggal 30 April 2003
+                        </p>
 
-                    <div class="mt-4 md:mt-8">
-                        <a href="#"
-                            class="inline-block rounded border border-white bg-white px-12 py-3 text-sm font-medium text-blue-500 transition hover:bg-transparent hover:text-white focus:outline-none focus:ring focus:ring-yellow-400">
-                            Get Started Today
-                        </a>
+                        <div class="mt-4 md:mt-8">
+                            <a href="#"
+                                class="inline-block rounded border border-white bg-white px-12 py-3 text-sm font-medium text-blue-500 transition hover:bg-transparent hover:text-white focus:outline-none focus:ring focus:ring-yellow-400">
+                                Get Started Today
+                            </a>
+                        </div>
                     </div>
                 </div>
+
+                <div class="grid grid-cols-2 gap-4 md:grid-cols-1 lg:grid-cols-2">
+                    <img alt="Student" src="{{asset('img/landing/landing (2).jpeg')}}"
+                        class="h-40 w-full object-cover sm:h-56 md:h-full" />
+
+                    <img alt="Student" src="{{asset('img/landing/landing (4).jpeg')}}"
+                        class="h-40 w-full object-cover sm:h-56 md:h-full" />
+                </div>
             </div>
+        </div>
+    </section>
 
-            <div class="grid grid-cols-2 gap-4 md:grid-cols-1 lg:grid-cols-2">
-                <img alt="Student" src="{{asset('img/landing/landing (2).jpeg')}}"
-                    class="h-40 w-full object-cover sm:h-56 md:h-full" />
-
-                <img alt="Student" src="{{asset('img/landing/landing (4).jpeg')}}"
-                    class="h-40 w-full object-cover sm:h-56 md:h-full" />
+    <section xmlns="http://www.w3.org/1999/xhtml" class="w-full py-16 bg-p-truegray-800">
+        <div class="container mx-auto flex flex-col-reverse md:flex-row items-start justify-center">
+            <div class="w-10/12 md:w-6/12 px-4 md:px-12 shadow-xl">
+                <h6 pazly-editable="innerHTML"
+                    class=" font-semibold whitespace-pre-line text-2xl text-p-white text-center md:text-left">
+                    Visi</h6>
+                <p pazly-editable="innerHTML" class=" text-lg whitespace-pre-line text-p-white text-justify">
+                    “Menjadi perusahaan yang mampu memberi manfaat dan peran terhadap kemajuan sektor industri usaha
+                    kecil menengah (UKM) pada umumnya dan sektor periwisata pada khususnya yang bertujuan untuk
+                    meningkatkan kesejahteraan para pelaku usaha industri sirup markisa dengan target mengeksport produk
+                    sampai ke beberapa negara” </p>
+            </div>
+            <div class="w-10/12 md:w-6/12 px-4 pb-6 md:px-12 bg-blueLight text-white">
+                <h6 pazly-editable="innerHTML"
+                    class="font-semibold whitespace-pre-line text-2xl text-p-white text-center md:text-left">
+                    Misi</h6>
+                <ul type="1" class="list-decimal">
+                    <li> Meningkatkan dan mempertahankan kualitas produk yang
+                        berkesinambungan </li>
+                    <li>
+                        Meningkatkan potensi SDM yang terlibat pada perusahaan khususnya
+                        karyawan UD. Mega Buana
+                    </li>
+                    <li> Menjalin hubungan baik antara sesama kompetitor dan pasar sebagai mitra
+                        dagang yang saling memberi manfaat </li>
+                </ul>
             </div>
         </div>
-    </div>
-</section>
-
-<section xmlns="http://www.w3.org/1999/xhtml" class="w-full py-16 bg-p-truegray-800">
-    <div class="container mx-auto flex flex-col-reverse md:flex-row items-start justify-center">
-        <div class="w-10/12 md:w-6/12 px-4 md:px-12 shadow-xl">
-            <h6 pazly-editable="innerHTML"
-                class=" font-semibold whitespace-pre-line text-2xl text-p-white text-center md:text-left">
-                Visi</h6>
-            <p pazly-editable="innerHTML" class=" text-lg whitespace-pre-line text-p-white text-justify">
-                “Menjadi perusahaan yang mampu memberi manfaat dan peran terhadap kemajuan sektor industri usaha
-                kecil menengah (UKM) pada umumnya dan sektor periwisata pada khususnya yang bertujuan untuk
-                meningkatkan kesejahteraan para pelaku usaha industri sirup markisa dengan target mengeksport produk
-                sampai ke beberapa negara” </p>
-        </div>
-        <div class="w-10/12 md:w-6/12 px-4 pb-6 md:px-12 bg-blueLight text-white">
-            <h6 pazly-editable="innerHTML"
-                class="font-semibold whitespace-pre-line text-2xl text-p-white text-center md:text-left">
-                Misi</h6>
-            <ul type="1" class="list-decimal">
-                <li> Meningkatkan dan mempertahankan kualitas produk yang
-                    berkesinambungan </li>
-                <li>
-                    Meningkatkan potensi SDM yang terlibat pada perusahaan khususnya
-                    karyawan UD. Mega Buana
-                </li>
-                <li> Menjalin hubungan baik antara sesama kompetitor dan pasar sebagai mitra
-                    dagang yang saling memberi manfaat </li>
-            </ul>
-        </div>
-    </div>
-</section>
-
-<!-- Pazly Scripts Start Here -->
-<section id="pazly-scripts-set" class="m-0 p-0 w-0 h-0">
-
-    <!-- Smooth scrolling -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script>
-        // handle links with @href started with '#' only
-    $(document).on('click', 'a[href^="#"]', function (e) {
-        // target element id
-        var id = $(this).attr('href');
-
-        // target element
-        var $id = $(id);
-        if ($id.length === 0) {
-            return;
-        }
-
-        // prevent standard hash navigation (avoid blinking in IE)
-        e.preventDefault();
-
-        // top position relative to the document
-        var pos = $id.offset().top;
-
-        // animated top scrolling
-        $('body, html').animate({
-            scrollTop: pos
-        });
-    });
-    </script>
-
-</section>
-
-
+    </section>
     <div class="w-full max-h-full block card">
         <div class="flex justify-center card-body items-center text-center ">
-        <div class="text-center text-base md:text-2xl lg:text-3xl font-bold underline">Khasiat dan Manfaat Buah Markisa</div>
+            <div class="text-center text-base md:text-2xl lg:text-3xl font-bold underline">Khasiat dan Manfaat Buah
+                Markisa</div>
             <div class="max-w-lg text-justify">
                 <p class="text-base md:text-lg lg:text-2xl ">
                     Markisa merupakan salah satu buah yang memiliki banyak manfaat bagi kesehatan tubuh karena kaya akan
-                vitamin
-                dan sejumlah mineral penting.
+                    vitamin
+                    dan sejumlah mineral penting.
 
-                Mengkonsumsi buah Markisa, atau produk olahan dari buah Markisa berwarna kuning secara rutin dan teratur
-                dapat memberikan manfaat sebagai berikut:
+                    Mengkonsumsi buah Markisa, atau produk olahan dari buah Markisa berwarna kuning secara rutin dan
+                    teratur
+                    dapat memberikan manfaat sebagai berikut:
                 </p>
-            <ul type="1" class=" list-decimal pl-4">
-                <li>Mencegah Pertumbuhan Sel Kanker</li>
-                <li>Memperlancar dan Menyehatkan Pencernaan</li>
-                <li>Memiliki Efek Relaksasi</li>
-                <li> Baik untuk Penderita Insomnia</li>
-                <li> Baik untuk Penderita Asma</li>
-                <li>Sumber Nutrisi yang Baik</li>
-                <li>Mengoptimalkan Sistem Imunitas</li>
-                <li>Mengontrol Tekanan Darah</li>
-                <li>Baik untuk Diet</li>
-                <li> Baik dalam Memenuhi Gizi Tubuh</li>
-                <li>Baik untuk Kesehatan Mental dan Otak</li>
-                <li>Baik untuk Kesehatan Kulit</li>
-                <li>Mampu Mendorong Pertumbuhan Tulang dan Gigi</li>
-                <li> Mencegah Infeksi</li>
-                <li>Menjaga Kesehatan Jantung</li>
-            </ul>
+                <ul type="1" class=" list-decimal pl-4">
+                    <li>Mencegah Pertumbuhan Sel Kanker</li>
+                    <li>Memperlancar dan Menyehatkan Pencernaan</li>
+                    <li>Memiliki Efek Relaksasi</li>
+                    <li> Baik untuk Penderita Insomnia</li>
+                    <li> Baik untuk Penderita Asma</li>
+                    <li>Sumber Nutrisi yang Baik</li>
+                    <li>Mengoptimalkan Sistem Imunitas</li>
+                    <li>Mengontrol Tekanan Darah</li>
+                    <li>Baik untuk Diet</li>
+                    <li> Baik dalam Memenuhi Gizi Tubuh</li>
+                    <li>Baik untuk Kesehatan Mental dan Otak</li>
+                    <li>Baik untuk Kesehatan Kulit</li>
+                    <li>Mampu Mendorong Pertumbuhan Tulang dan Gigi</li>
+                    <li> Mencegah Infeksi</li>
+                    <li>Menjaga Kesehatan Jantung</li>
+                </ul>
             </div>
         </div>
     </div>
     <div class="w-full max-h-full block mt-4 card ">
         <div class="flex justify-center ">
             <div class="max-w-xl card-body items-center text-center">
-                <div class="text-base md:text-2xl lg:text-3xl  font-bold underline card-title">Cara Order Sari Markisa MegaBuana Makassar</div>
+                <div class="text-base md:text-2xl lg:text-3xl  font-bold underline card-title">Cara Order Sari Markisa
+                    MegaBuana Makassar</div>
                 <p class="text-base md:text-lg lg:text-2xl ">
-                    Untuk memesan produk Sari Buah Markisa Karaeng 100% ASLI dari Makassar, silahkan hubungi Telp. 0822-9059-1298 setiap hari kerja.
+                    Untuk memesan produk Sari Buah Markisa Karaeng 100% ASLI dari Makassar, silahkan hubungi Telp.
+                    0822-9059-1298 setiap hari kerja.
                 </p>
             </div>
         </div>
