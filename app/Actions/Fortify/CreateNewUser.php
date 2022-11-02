76f9863014840814c45652e64e6fccf5d897293b
thype5209 <44895587+thype5209@users.noreply.github.com>
2022-11-02 17:42:27 +0800

    Update NewUser

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -34,6 +34,7 @@ class CreateNewUser implements CreatesNewUsers
                 'name' => $input['name'],
                 'email' => $input['email'],
                 'password' => Hash::make($input['password']),
+                'role_id'=> '1',
             ]), function (User $user) {
                 $this->createTeam($user);
             });
