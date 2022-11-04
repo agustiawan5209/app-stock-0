d0678df330ba7362bc2487b8cbb9dceb7af6861a
thype5209 <44895587+thype5209@users.noreply.github.com>
2022-11-04 21:29:34 +0800

    Buat Model BahanBaku Kemasan Supplier

---
+++ app/Models/BahanBakuKemasanSupplier.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class BahanBakuKemasanSupplier extends Model
+{
+    use HasFactory;
+    protected $table = 'bahan_baku_kemasan_suppliers';
+    protected $fillable = ['gambar','bahan_baku','satuan' ,'isi', 'harga','jumlah_stock', 'suppliers_id'];
+
+    public function supplier()
+    {
+        return $this->belongsTo(Supplier::class, 'suppliers_id');
+    }
+    public function barang_masuk()
+    {
+        return $this->hasOne(BarangMasuk::class, 'bahan');
+    }
+    public function bahanbaku(){
+        return $this->hasOne(BahanBakuKemasan::class, 'id', 'bahan_baku');
+    }
+}
---
+++ database/migrations/2022_11_04_212741_create_bahan_baku_kemasan_suppliers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bahan_baku_kemasan_suppliers', function (Blueprint $table) {
+            $table->id();
+            $table->string('gambar', 255)->nullable();
+            $table->string('bahan_baku', 50);
+            $table->string('isi')->nullable();
+            $table->string('satuan');
+            $table->integer('harga');
+            $table->integer('jumlah_stock');
+            $table->foreignId('suppliers_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bahan_baku_kemasan_suppliers');
+    }
+};
