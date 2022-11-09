81f934b4c4cfb37cb9d4dffc34438e4cb5e25394
thype5209 <44895587+thype5209@users.noreply.github.com>
2022-11-09 20:50:29 +0800

    Update Guest

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,7 +11,7 @@
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
     @livewireStyles
-@vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="{{asset('build/assets/app.e4f635fb.css')}}">
     <!-- Styles -->
 </head>
 
