c1a48c9c289b5f15fa5bf0600b26606a330c5509
thype5209 <44895587+thype5209@users.noreply.github.com>
2022-11-09 20:57:16 +0800

    Update Pagebank

--- resources/views/livewire/transaksi/page-bank.blade.php
+++ resources/views/livewire/transaksi/page-bank.blade.php
@@ -229,7 +229,7 @@
 
         <div class="w-full overflow-hidden rounded-lg shadow-xs">
             <div class="w-full overflow-x-auto">
-                <x-table id="MetodeTable" class="w-full whitespace-no-wrap">
+                <x-table :tambahItem="true" id="MetodeTable" class="w-full whitespace-no-wrap">
                     <thead>
                         <x-tr
                             class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b   bg-gray-50      ">
