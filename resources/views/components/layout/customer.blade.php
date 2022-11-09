9c653e9253192dac2ec03654fbfc2034f5a66f2a
thype5209 <44895587+thype5209@users.noreply.github.com>
2022-11-09 21:28:37 +0800

    Update Customer

--- resources/views/components/layout/customer.blade.php
+++ resources/views/components/layout/customer.blade.php
@@ -54,7 +54,17 @@
           </ul>
         </div>
         <div class="navbar-end">
-          <a class="btn">Logout</a>
+            <form action="{{ route('logout') }}" method="POST"
+            class=" flex items-center space-x-2 py-1  group hover:border-r-2 hover:border-r-primary hover:font-semibold ">
+            @csrf
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1">
+                </path>
+            </svg>
+            <button type="submit">Logout</button>
+        </form>
         </div>
       </div>
       <main class=" ">
