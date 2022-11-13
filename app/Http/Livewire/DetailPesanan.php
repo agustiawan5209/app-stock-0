3335e0ef35d65c012e08e571a87bb1e8c7c1708e
thype5209 <44895587+thype5209@users.noreply.github.com>
2022-11-13 21:26:24 +0800

    Update DetailPesanan

--- app/Http/Livewire/DetailPesanan.php
+++ app/Http/Livewire/DetailPesanan.php
@@ -14,7 +14,6 @@ class DetailPesanan extends Component
     public function render()
     {
         $barang = BarangMasuk::with(['pesanan'])->find($this->itemID);
-        dd($barang);
         return view('livewire.detail-pesanan', [
             'barang'=> $barang,
         ])->layoutData(['page'=> 'Detail Pesanan ']);
