e702d47824a7cf43d39ddc6d86a7cb7978010fcd
thype5209 <44895587+thype5209@users.noreply.github.com>
2022-11-16 00:22:58 +0800

    Cek Relasi jenis Laporanbarangmasuk

--- app/Http/Livewire/Laporan/LaporanBarangMasuk.php
+++ app/Http/Livewire/Laporan/LaporanBarangMasuk.php
@@ -10,9 +10,9 @@ class LaporanBarangMasuk extends Component
     public $tgl_awal,$tgl_akhir, $cetak = false;
     public function render()
     {
-        $barangmasuk = BarangMasuk::where('status', '=', 4)->get();
+        $barangmasuk = BarangMasuk::with(['pesanan', 'pesanan.transaksi'])->where('status', '=', 4)->get();
         if($this->cetak == true){
-            $barangmasuk = BarangMasuk::where('status', '=', 4)
+            $barangmasuk = BarangMasuk::with(['pesanan', 'pesanan.transaksi'])->where('status', '=', 4)
             ->whereHas('pesanan', function($query){
                 return $query->whereHas('transaksi', function($query){
                     return $query->whereBetween('tgl_transaksi', [$this->tgl_awal, $this->tgl_akhir]);
