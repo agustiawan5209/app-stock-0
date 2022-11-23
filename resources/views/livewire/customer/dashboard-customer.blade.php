de49ac28787d59ff1eab433505948699bc958c15
Agustiawan <44895587+thype5209@users.noreply.github.com>
2022-11-23 16:10:49 +0800

    Update dashboard customer

--- resources/views/livewire/customer/dashboard-customer.blade.php
+++ resources/views/livewire/customer/dashboard-customer.blade.php
@@ -1,7 +1,7 @@
 <div>
     <section class=" container mt-10 mx-auto">
         @include('sweetalert::alert')
-        <div class="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-4">
+        <div class="grid gap-6 mb-8 md:grid-cols-2 ">
             <!-- Card -->
             <div class="flex items-center p-4 bg-white rounded-lg shadow-xs">
                 <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full ">
@@ -36,42 +36,6 @@
                     </p>
                     <p class="text-lg font-semibold text-gray-700 ">
                         Rp. {{number_format($pembelian,0,2)}}
-                    </p>
-                </div>
-            </div>
-            <!-- Card -->
-            <div class="flex items-center p-4 bg-white rounded-lg shadow-xs">
-                <div class="p-3 mr-4 text-blue-500 bg-blue-100 rounded-full      ">
-                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                        <path
-                            d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z">
-                        </path>
-                    </svg>
-                </div>
-                <div>
-                    <p class="mb-2 text-sm font-medium text-gray-600 ">
-                        Total Penjualan Bulan Ini
-                    </p>
-                    <p class="text-lg font-semibold text-gray-700 ">
-                        {{-- {{ $total_penjualan }} Dus --}}
-                    </p>
-                </div>
-            </div>
-            <!-- Card -->
-            <div class="flex items-center p-4 bg-white rounded-lg shadow-xs">
-                <div class="p-3 mr-4 text-teal-500 bg-teal-100 rounded-full   ">
-                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
-                        <path fill-rule="evenodd"
-                            d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z"
-                            clip-rule="evenodd"></path>
-                    </svg>
-                </div>
-                <div>
-                    <p class="mb-2 text-sm font-medium text-gray-600 ">
-                        Status Pemesanan Bahan Baku Belum Di Konfirmasi
-                    </p>
-                    <p class="text-lg font-semibold text-gray-700 ">
-                        {{-- {{$pesan}} --}}
                     </p>
                 </div>
             </div>
