0942a8dec9ae1bbfa1c8dec200ed906456e00844
Agustiawan <44895587+thype5209@users.noreply.github.com>
2023-01-21 15:48:32 +0800

    update models dan migration database

--- app/Models/BahanBaku.php
+++ app/Models/BahanBaku.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class BahanBaku extends Model
 {
     use HasFactory;
+    use SoftDeletes;
     protected $table = 'bahan_bakus';
     protected $fillable = [
         'nama_bahan_baku',
--- database/migrations/2022_08_28_111915_create_bahan_bakus_table.php
+++ database/migrations/2022_08_28_111915_create_bahan_bakus_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('bahan_bakus', function (Blueprint $table) {
             $table->id();
             $table->string('nama_bahan_baku', 30);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
