3ee6e58a4b6732847f8f59a62430e7162079c303
Agustiawan <44895587+thype5209@users.noreply.github.com>
2023-05-12 15:36:44 +0800

    update laporanvarangmasuk.php

--- app/Http/Livewire/Laporan/LaporanBarangMasuk.php
+++ app/Http/Livewire/Laporan/LaporanBarangMasuk.php
@@ -10,9 +10,9 @@ class LaporanBarangMasuk extends Component
     public $tgl_awal,$tgl_akhir, $cetak = false;
     public function render()
     {
-        $barangmasuk = BarangMasuk::with(['pesanan', 'pesanan.transaksi'])->where('status', '=', 4)->get();
+        $barangmasuk = BarangMasuk::with(['pesanan', 'pesanan.transaksi'])->where('status', '=', '5')->get();
         if($this->cetak == true){
-            $barangmasuk = BarangMasuk::with(['pesanan', 'pesanan.transaksi'])->where('status', '=', 4)
+            $barangmasuk = BarangMasuk::with(['pesanan', 'pesanan.transaksi'])->where('status', '=', '5')
             ->whereHas('pesanan', function($query){
                 return $query->whereHas('transaksi', function($query){
                     return $query->whereBetween('tgl_transaksi', [$this->tgl_awal, $this->tgl_akhir]);
