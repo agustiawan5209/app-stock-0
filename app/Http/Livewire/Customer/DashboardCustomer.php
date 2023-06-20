2c452137b75bb1a6693c8e1cedebf97a3b60579a
Agustiawan <44895587+thype5209@users.noreply.github.com>
2023-06-21 07:17:58 +0800

    update dashboard customer

--- app/Http/Livewire/Customer/DashboardCustomer.php
+++ app/Http/Livewire/Customer/DashboardCustomer.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Livewire\Customer;
 
+use App\Models\Jenis;
 use Livewire\Component;
 use App\Models\StokProduk;
 use App\Models\PesananUser;
+use App\Models\ProdukFermentasi;
 use Illuminate\Support\Facades\Auth;
 use RealRashid\SweetAlert\Facades\Alert;
-use App\Models\ProdukFermentasi;
 
 class DashboardCustomer extends Component
 {
@@ -15,9 +16,19 @@ class DashboardCustomer extends Component
     {
         $stokProduk = StokProduk::latest()->first();
         $jumlah_pembelian = PesananUser::where('user_id', Auth::user()->id)->sum('sub_total');
+
+        $jenis_produk = Jenis::all();
+        $data = [];
+        foreach ($jenis_produk as $key => $value) {
+            $stok = StokProduk::where('jenis', $value->nama_jenis)->first();
+            if($stok !== null){
+                $data[] = StokProduk::where('jenis', $value->nama_jenis)->first();
+            }
+        }
         return view('livewire.customer.dashboard-customer',[
             'stok_produk'=> $stokProduk == null ?  0 : $stokProduk->jumlah_produksi,
             'pembelian'=> $jumlah_pembelian,
+            'data'=> $data,
         ])
         ->layout('components.layout.customer')
         ->layoutData(['page'=> 'Dashboard ']);
--- resources/views/livewire/customer/dashboard-customer.blade.php
+++ resources/views/livewire/customer/dashboard-customer.blade.php
@@ -3,24 +3,26 @@
         @include('sweetalert::alert')
         <div class="grid gap-6 mb-8 md:grid-cols-2 ">
             <!-- Card -->
-            <div class="flex items-center p-4 bg-white rounded-lg shadow-xs">
-                <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full ">
-                    <svg class="w-6 h-6 text-primary " fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4">
-                        </path>
-                    </svg>
+            @foreach ($data as $item)
+                <div class="flex items-center p-4 bg-white rounded-lg shadow-xs">
+                    <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full ">
+                        <svg class="w-6 h-6 text-primary " fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4">
+                            </path>
+                        </svg>
+                    </div>
+                    <div>
+                        <p class="mb-2 text-xs font-medium text-gray-600 ">
+                            Jumlah Stok {{ $item->jenis }}
+                        </p>
+                        <p class="text-lg font-semibold text-gray-700 ">
+                            {{ $item->jumlah_produksi }} Botol
+                        </p>
+                    </div>
                 </div>
-                <div>
-                    <p class="mb-2 text-xs font-medium text-gray-600 ">
-                        Jumlah Stok Manufactur
-                    </p>
-                    <p class="text-lg font-semibold text-gray-700 ">
-                        {{$stok_produk}} Liter
-                    </p>
-                </div>
-            </div>
+            @endforeach
             <!-- Card -->
             <div class="flex items-center p-4 bg-white rounded-lg shadow-xs">
                 <div class="p-3 mr-4 text-green-500 bg-green-100 rounded-full      ">
@@ -35,7 +37,7 @@
                         Total Pembelian Bulan Ini
                     </p>
                     <p class="text-lg font-semibold text-gray-700 ">
-                        Rp. {{number_format($pembelian,0,2)}}
+                        Rp. {{ number_format($pembelian, 0, 2) }}
                     </p>
                 </div>
             </div>
@@ -43,12 +45,14 @@
     </section>
     <div class="hero min-h-max bg-base-200">
         <div class="hero-content text-center">
-          <div class="max-w-md">
-            <h1 class="text-5xl font-bold">Selamat Datang {{ Auth::user()->name }}</h1>
-            <p class="py-6">UD. Mega Buana adalah perusahaan yang bergerak dalam industri pengolahan markisa. UD. Mega Buana pertama kali didirikan oleh Bapak IdrisHusain, SE. Nama UD. Mega Buana terdiri dari dua kata yaitu mega yang berarti besar dan buana yang berarti bumi.</p>
-            <a href="{{ route('Customer.Pesan-Produk') }}">
-                <button class="btn btn-primary">Lihat Produk</button></a>
-          </div>
+            <div class="max-w-md">
+                <h1 class="text-5xl font-bold">Selamat Datang {{ Auth::user()->name }}</h1>
+                <p class="py-6">UD. Mega Buana adalah perusahaan yang bergerak dalam industri pengolahan markisa. UD.
+                    Mega Buana pertama kali didirikan oleh Bapak IdrisHusain, SE. Nama UD. Mega Buana terdiri dari dua
+                    kata yaitu mega yang berarti besar dan buana yang berarti bumi.</p>
+                <a href="{{ route('Customer.Pesan-Produk') }}">
+                    <button class="btn btn-primary">Lihat Produk</button></a>
+            </div>
         </div>
-      </div>
+    </div>
 </div>
